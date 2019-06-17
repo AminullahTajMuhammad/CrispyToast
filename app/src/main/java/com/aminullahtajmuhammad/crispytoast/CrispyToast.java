7011c4a58a88efce44a18890ba563915310673cb
amin <aminullahtajmuhammad@gmail.com>
2019-06-18 02:27:00 +0500

    Created functionality of toast now convert in library

--- app/src/main/java/com/aminullahtajmuhammad/crispytoast/CrispyToast.java
+++ app/src/main/java/com/aminullahtajmuhammad/crispytoast/CrispyToast.java
@@ -3,6 +3,11 @@ package com.aminullahtajmuhammad.crispytoast;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CrispyToast extends Toast {
@@ -13,15 +18,81 @@ public class CrispyToast extends Toast {
      * @param context The context to use.  Usually your {@link Application}
      *                or {@link Activity} object.
      */
+    public static int DEFAULT_STATE = 1;
+    public static int ERROR_STATE = 2;
+    public static int SUCCESS_STATE = 3;
+    public static int WARNING_STATE = 4;
+    public static int CONFUSE_STATE = 5;
+    public static int CONNECTION_LOST_STATE = 6;
+
+
     CrispyToast(Context context) { super(context); }
 
-    public Toast makeCrispyToast(Context context, String text, int type, int duration) {
+    public static Toast makeCrispyToast(Context context, String text, int type, int duration) {
+
+        RelativeLayout crispyToastLayout;
+        TextView crispyToastText;
+        ImageView crispyToastImage;
+
+        View toastLayout = LayoutInflater.from(context).inflate(R.layout.crispy_toast_layout, null, false);
+
+        crispyToastLayout = toastLayout.findViewById(R.id.crispyToastLayout);
+        crispyToastImage = toastLayout.findViewById(R.id.crispyToastIcon);
+        crispyToastText = toastLayout.findViewById(R.id.crispyToastText);
+
         Toast toast = new Toast(context);
 
-        // Set Toast Duration
+        // Set CrispyToast Duration
         toast.setDuration(duration);
 
+        // Set CrispyToast Text
+        crispyToastText.setText(text);
 
+        // Set CrispyToast Type
+        switch (type) {
+            case 1: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_default_state);
+                crispyToastImage.setVisibility(View.GONE);
+                break;
+            }
+
+            case 2: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_error_state);
+                crispyToastImage.setImageResource(R.drawable.ic_clear_black_24dp);
+                break;
+            }
+
+            case 3: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_success_state);
+                crispyToastImage.setImageResource(R.drawable.ic_check_black_24dp);
+                break;
+            }
+
+            case 4: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_warning_state);
+                crispyToastImage.setImageResource(R.drawable.ic_info_outline_black_24dp);
+                break;
+            }
+
+            case 5: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_confuse_state);
+                crispyToastImage.setImageResource(R.drawable.ic_pan_tool_black_24dp);
+
+                break;
+            }
+
+            case 6: {
+                crispyToastLayout.setBackgroundResource(R.drawable.shape_connect_lost_state);
+                crispyToastImage.setImageResource(R.drawable.ic_refresh_black_24dp);
+
+                break;
+            }
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
+
+        toast.setView(toastLayout);
         return toast;
     }
 }
--- app/src/main/java/com/aminullahtajmuhammad/crispytoast/MainActivity.java
+++ app/src/main/java/com/aminullahtajmuhammad/crispytoast/MainActivity.java
@@ -3,16 +3,90 @@ package com.aminullahtajmuhammad.crispytoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    CrispyToast crispyToast = new CrispyToast(this);
+    Button btnDefault, btnSuccess, btnError, btnWarning, btnConnectLost, btnConfuse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        crispyToast.makeCrispyToast(this, "This is toast", CrispyToast.ERROR, CrispyToast.LENGTH_SHORT);
+        btnDefault = findViewById(R.id.btnDefault);
+        btnConfuse = findViewById(R.id.btnConfuse);
+        btnConnectLost = findViewById(R.id.btnConnectLost);
+        btnError = findViewById(R.id.btnError);
+        btnSuccess = findViewById(R.id.btnSuccess);
+        btnWarning = findViewById(R.id.btnWarning);
+
+        btnDefault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Default Toast",
+                        CrispyToast.DEFAULT_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnConfuse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Confuse Toast",
+                        CrispyToast.CONFUSE_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnConnectLost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Connection Lost Toast",
+                        CrispyToast.CONNECTION_LOST_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Error Toast",
+                        CrispyToast.ERROR_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnSuccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Success Toast",
+                        CrispyToast.SUCCESS_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
+        btnWarning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CrispyToast.makeCrispyToast(
+                        MainActivity.this,
+                        "This is Warning Toast",
+                        CrispyToast.WARNING_STATE,
+                        CrispyToast.LENGTH_SHORT
+                ).show();
+            }
+        });
     }
 }
